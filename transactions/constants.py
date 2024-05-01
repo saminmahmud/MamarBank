3d2b78eaf1bc7ff136d0c514108740aee8292d36
saminmahmud <samin.mahmud18@gmail.com>
2024-05-02 01:00:58 +0600

    mamar_bank

---
+++ transactions/constants.py
@@ -0,0 +1,16 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+MONEY_TRANSFER = 5
+MONEY_RECEIVED = 6
+
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+    (MONEY_TRANSFER, 'MONEY TRANSFER'),
+    (MONEY_RECEIVED, 'MONEY RECEIVED'),
+    
+)
